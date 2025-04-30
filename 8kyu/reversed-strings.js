1d633bc7dc222171efc62a394a96d26defd7c9a8
JuliaGolota <golotajulia@gmail.com>
2025-04-30 23:47:48 +0300

    completed reversed-strings.js

---
+++ 8kyu/reversed-strings.js
@@ -0,0 +1,25 @@
+// 📌DESCRIPTIONS
+// Complete the solution so that it reverses the string passed into it.
+
+
+//  👩‍💻🧐TESTS
+// assert.strictEqual(solution('world'), 'dlrow');
+//     assert.strictEqual(solution('hello'), 'olleh');
+//     assert.strictEqual(solution(''), '');
+//     assert.strictEqual(solution('h'), 'h');
+
+
+
+// ✅SOLUTION
+function solution(str){
+  let newStr = '';
+  for (let i = str.length - 1; i >= 0 ; i--) { 
+    newStr += (str[i]);    
+  }
+  return newStr;
+}
+console.log(solution('world'));
+console.log(solution('hello'));
+console.log(solution(''));
+console.log(solution('h'));
+
